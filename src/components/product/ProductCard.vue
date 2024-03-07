c3aaecf1e35b44a2f48ead221df1e1193e17763c
Тимур <kilkenny116@gmail.com>
2024-03-07 19:09:23 +0300

    up

--- src/components/product/ProductCard.vue
+++ src/components/product/ProductCard.vue
@@ -11,13 +11,34 @@
           <span class="product__card-price-title">Цена:</span>
           <span class="product__card-price-value">12 999 руб.</span>
         </div>
-        <div class="product__card-add-product"></div>
+        <div class="product__card-add-product">
+          <div class="border">
+            <span> + </span>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <style lang="scss">
+.border {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 32px;
+  height: 32px;
+  border-radius: 8px;
+  border: 1px solid #f2f2f2;
+  color: #d3d3d3;
+  cursor: pointer;
+  background: transparent;
+
+  &_active {
+    background: linear-gradient(180deg, #89f09c 0%, #3cc755 100%);
+  }
+}
+
 .product {
   &__card-wrapper {
     padding: 20px 20px 34px;
@@ -34,8 +55,14 @@
   &__card-price {
     display: flex;
     flex-direction: column;
-    padding-top: 14px;
     gap: 2px;
+  }
+
+  &__card-footer {
+    display: flex;
+    align-items: end;
+    justify-content: space-between;
+    padding-top: 14px;
   }
 
   &__card-price-title {
