2c97f41edeef3052dd10176fa9c25d032e21b79a
Тимур <kilkenny116@gmail.com>
2024-05-17 18:46:55 +0300

    skelet component

--- src/components/product/ProductCards.vue
+++ src/components/product/ProductCards.vue
@@ -1,28 +1,61 @@
-<script setup>
+<script setup lang="ts">
+import { ref, type Ref } from 'vue'
 import ProductCard from '@components/product/ProductCard.vue'
+import ProductSkelet from '@components/product/ProductSkelet.vue'
+
+const loader: Ref<boolean> = ref(true)
+
+setTimeout(() => {
+  loader.value = false
+}, 1000)
+
 const test = [
   {
     id: 1,
     title: 'Мужские Кроссовки Nike Blazer Mid Suede',
-    price: '1299132d329',
+    price: 321123,
     image: '@img/product-1.png'
   },
   {
     id: 2,
     title: 'Мужские Кроссовки Nike Blazer Mid Suede',
-    price: '1299132d329',
+    price: 321123,
     image: '@img/product-1.png'
   },
   {
     id: 3,
     title: 'Мужские Кроссовки Nike Blazer Mid Suede',
-    price: '1299132d329',
+    price: 321123,
     image: '@img/product-1.png'
   },
   {
     id: 4,
     title: 'Мужские Кроссовки Nike Blazer Mid Suede',
-    price: '1299132d329',
+    price: 321123,
+    image: '@img/product-1.png'
+  },
+  {
+    id: 5,
+    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+    price: 321123,
+    image: '@img/product-1.png'
+  },
+  {
+    id: 6,
+    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+    price: 321123,
+    image: '@img/product-1.png'
+  },
+  {
+    id: 7,
+    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+    price: 321123,
+    image: '@img/product-1.png'
+  },
+  {
+    id: 8,
+    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+    price: 321123,
     image: '@img/product-1.png'
   }
 ]
@@ -30,7 +63,14 @@ const test = [
 
 <template>
   <div class="product__items">
-    <ProductCard v-for="item in test" :image="item.image" :price="item.price" :id="item.id" />
+    <ProductCard
+      v-for="item in test"
+      :image="item.image"
+      :price="item.price"
+      :id="item.id"
+      v-if="!loader"
+    />
+    <ProductSkelet v-for="item in 12" v-else />
   </div>
 </template>
 
