8073375cb8972e094f2412b48bfe9d8e7db79f85
Тимур <kilkenny116@gmail.com>
2024-05-17 18:47:37 +0300

    up

--- src/components/TheProducts.vue
+++ src/components/TheProducts.vue
@@ -1,12 +1,24 @@
-<script setup>
+<script setup lang="ts">
+import { ref, type Ref } from 'vue'
+import IconSearch from '@components/icons/IconSearch.vue'
 import ProductCards from '@components/product/ProductCards.vue'
+import Input from '@components/UI/Input.vue'
+
+const search: Ref<string> = ref('')
 </script>
 
 <template>
   <div class="product">
-    <div class="product__title">Все кроссовки</div>
+    <div class="product__head">
+      <div class="product__title">Все кроссовки</div>
+      <Input v-model:value="search" placeholder="Поиск" :value="search">
+        <template v-slot:before>
+          <IconSearch class="icon-search" />
+        </template>
+      </Input>
+    </div>
+    <ProductCards />
   </div>
-  <ProductCards />
 </template>
 
 <style lang="scss">
@@ -16,5 +28,18 @@ import ProductCards from '@components/product/ProductCards.vue'
     line-height: 39px;
     font-size: 32px;
   }
+
+  &__head {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+}
+
+.icon-search {
+  position: absolute;
+  left: 20px;
+  top: 50%;
+  transform: translateY(-50%);
 }
 </style>
--- src/stores/basket.ts
+++ src/stores/basket.ts
@@ -1,9 +1,12 @@
 import { ref, computed, type Ref } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useBasketStore = defineStore('basket', () => {
+interface IBasket {
+  id: number
+}
 
-  const basket: Ref<[object]>  = ref([])
+export const useBasketStore = defineStore('basket', () => {
+  const basket: Ref<Array<IBasket>> = ref([])
 
   function addProduct(id: number): boolean {
     if (basket.value.findIndex((e) => e.id === id) === -1) {
@@ -14,9 +17,21 @@ export const useBasketStore = defineStore('basket', () => {
     return false
   }
 
-  function removeProduct(id: number) : boolean {
+  const checkIssetProduct = (id: number) => {
+    if (basket.value.findIndex((e) => e.id === id) !== -1) {
+      return true
+    }
+
+    return false
+  } 
+
+  const resetBasket = () => {
+    basket.value = []
+  } 
+
+  function removeProduct(id: number): boolean {
     if (basket.value.find((e) => e.id === id)) {
-      basket.value.push({ id: id })
+      basket.value.splice(basket.value.findIndex((e) => e.id === id), 1)
       return true
     }
 
