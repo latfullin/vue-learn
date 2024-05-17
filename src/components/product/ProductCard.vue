68973867396eafe26949a5657d66f55500bda11d
Тимур <kilkenny116@gmail.com>
2024-05-17 18:47:08 +0300

    fix style

--- src/components/product/ProductCard.vue
+++ src/components/product/ProductCard.vue
@@ -1,25 +1,42 @@
 <script setup lang="ts">
 import { useBasketStore } from '@stores/basket'
-
-const { addProduct } = useBasketStore()
+import { ref, watch } from 'vue'
+import IconFavorite from '../icons/IconFavorite.vue'
+const { addProduct, basket, removeProduct } = useBasketStore()
 
 const props = defineProps<{
   id: number
   title: string
-  price: object
-  image: string
+  price: string
+  image: number
 }>()
 
-const productInBasket = true
+const productInBasket = ref(true)
+
+interface IBasket {
+  id: number
+}
+
+watch(basket, (e) => {
+  if (e.find((element: IBasket) => element.id === props.id)) {
+    productInBasket.value = false
+  } else {
+    productInBasket.value = true
+  }
+})
 </script>
 
 <template>
   <div class="product__card">
     <div class="product__card-wrapper">
+      <IconFavorite
+        class="product__favorite"
+        :background="!productInBasket ? '#FEF0F0' : 'transperent'"
+        :heart="!productInBasket ? '#FF8585' : 'transperent'"
+      />
       <div class="product__card-head">
-        <img :src="image" alt="" />
+        <img src="@img/product-1.png" alt="" />
       </div>
-
       <div class="product__card-body">Мужские Кроссовки Nike Blazer Mid Suede</div>
       <div class="product__card-footer">
         <div class="product__card-price">
@@ -27,12 +44,16 @@ const productInBasket = true
           <span class="product__card-price-value">{{ price }} руб.</span>
         </div>
         <div class="product__card-add-product">
-          <div class="border" v-if="productInBasket" @click="() => addProduct(id)">
+          <button
+            class="product__card-increment"
+            v-if="productInBasket"
+            @click="() => addProduct(id)"
+          >
             <span> + </span>
-          </div>
-          <div class="success" v-else>
+          </button>
+          <button class="success" v-else @click="() => removeProduct(id)">
             <img src="@img/product-in-basket.svg" alt="" />
-          </div>
+          </button>
         </div>
       </div>
     </div>
@@ -40,30 +61,14 @@ const productInBasket = true
 </template>
 
 <style lang="scss">
-.border {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 32px;
-  height: 32px;
-  border-radius: 8px;
-  border: 1px solid #f2f2f2;
-  color: #d3d3d3;
-  cursor: pointer;
-  transition: box-shadow 0.3s;
-  background: transparent;
-
-  &_active {
-    background: linear-gradient(180deg, #89f09c 0%, #3cc755 100%);
-  }
-}
-
 .success {
   position: relative;
   height: 32px;
   width: 32px;
   border-radius: 8px;
   background: linear-gradient(180deg, #89f09c 0%, #3cc755 100%);
+  outline: none;
+  border: none;
 
   img {
     position: absolute;
@@ -76,11 +81,50 @@ const productInBasket = true
 }
 
 .product {
+  &__favorite {
+    position: absolute;
+    left: 30px;
+    top: 30px;
+    opacity: 0;
+    transition: opacity 0.6s;
+    cursor: pointer;
+  }
+
+  &__card:hover .product__favorite {
+    opacity: 1;
+  }
+
   &__card-wrapper {
+    position: relative;
     padding: 20px 20px 34px;
     border: 1px solid #f3f3f3;
     border-radius: 40px;
+    transition: box-shadow 0.3s;
     @extend %default-box-shadow;
+  }
+
+  &__card-increment {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 32px;
+    height: 32px;
+    border-radius: 8px;
+    border: 1px solid #f2f2f2;
+    color: #d3d3d3;
+    cursor: pointer;
+    transition:
+      box-shadow 0.3s,
+      background 0.3s;
+    background: transparent;
+
+    &:hover {
+      background: linear-gradient(180deg, #89f09c 0%, #3cc755 100%);
+    }
+
+    &_active {
+      background: linear-gradient(180deg, #89f09c 0%, #3cc755 100%);
+    }
   }
 
   &__card-price {
