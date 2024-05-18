e2fb743af834895f47c166f206071336e5775751
Тимур <kilkenny116@gmail.com>
2024-05-18 15:05:08 +0300

    basket store

--- src/stores/basket.ts
+++ src/stores/basket.ts
@@ -2,41 +2,52 @@ import { ref, computed, type Ref } from 'vue'
 import { defineStore } from 'pinia'
 
 interface IBasket {
-  id: number
+  id: number,
+  price: number
 }
 
-export const useBasketStore = defineStore('basket', () => {
-  const basket: Ref<Array<IBasket>> = ref([])
+export const useBasketStore = defineStore({
+  id: 'basket',
+  // const basket: Ref<Array<IBasket>> = ref([])
+  state: () => ({
+    basket: [] as Array<IBasket>
+  }),
 
-  function addProduct(id: number): boolean {
-    if (basket.value.findIndex((e) => e.id === id) === -1) {
-      basket.value.push({ id: id })
-      return true
-    }
+  getters: {
+    amount: (state) => state.basket.reduce((acc,e) => acc + e.price, 0),
+  },
 
-    return false
-  }
+  actions: {
+    addProduct(id: number, price: number): boolean {
+      if (this.basket.findIndex((e: IBasket) => e.id === id) === -1) {
+        this.basket.push({ id: id, price })
+        return true
+      }
+  
+      return false
+    },
+  
+     checkIssetProduct (id: number): boolean {
+      if (this.basket.findIndex((e: IBasket) => e.id === id) !== -1) {
+        return true
+      }
+  
+      return false
+    },
+     
+     resetBasket () :void {
+      this.basket = []
+    } ,
+  
+    removeProduct(id: number): boolean {
+      if (this.basket.find((e: IBasket) => e.id === id)) {
+        this.basket.splice(this.basket.findIndex((e: IBasket) => e.id === id), 1)
+        return true
+      }
+  
+      return false
+    },
+  },
 
-  const checkIssetProduct = (id: number) => {
-    if (basket.value.findIndex((e) => e.id === id) !== -1) {
-      return true
-    }
 
-    return false
-  } 
-
-  const resetBasket = () => {
-    basket.value = []
-  } 
-
-  function removeProduct(id: number): boolean {
-    if (basket.value.find((e) => e.id === id)) {
-      basket.value.splice(basket.value.findIndex((e) => e.id === id), 1)
-      return true
-    }
-
-    return false
-  }
-
-  return { basket, addProduct, removeProduct }
 })
