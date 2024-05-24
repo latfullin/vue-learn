a56746e6afe639606f16fd2080ad339157318216
Тимур <kilkenny116@gmail.com>
2024-05-24 10:21:48 +0300

    moved components the sidebar in layouts

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import TheHeader from './components/TheHeader.vue'
+import TheSidebar from '@components/TheSidebar.vue'
 </script>
 
 <template>
   <TheHeader />
   <main>
     <RouterView />
+    <TheSidebar />
   </main>
 </template>
 
