0331a02efe3cf9e9336078103234199a637b294b
Тимур <kilkenny116@gmail.com>
2024-05-26 14:50:49 +0300

    page favorites

---
+++ src/components/icons/IconBack.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import IconArrow from './IconArrow.vue'
+</script>
+
+<template>
+  <div class="icon-back">
+    <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M6 11L1 6L6 1"
+        stroke="#C8C8C8"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.icon-back {
+  position: relative;
+  border: 1px solid #f2f2f2;
+  width: 35px;
+  height: 35px;
+  cursor: pointer;
+  border-radius: 8px;
+
+  svg {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+}
+</style>
--- src/views/FavoriteView.vue
+++ src/views/FavoriteView.vue
@@ -1,3 +1,46 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import IconBack from '@components/icons/IconBack.vue'
+import ProductCards from '@components/product/ProductCards.vue'
+import Title from '@components/UI/Title.vue'
 
-<template>dsdasdasa</template>
+import { useProductStore } from '@/stores/products'
+import { useBasketStore } from '@/stores/basket'
+import { onMounted } from 'vue'
+import { ref } from 'vue'
+import { watch } from 'vue'
+
+const products: Product = ref([])
+
+const { getItems } = useProductStore()
+const { favorites } = useBasketStore()
+
+onMounted(() => {
+  products.value = getItems(favorites)
+})
+
+watch(favorites, () => {
+  products.value = getItems(favorites)
+})
+</script>
+
+<template>
+  <div class="favorite">
+    <Title class="favorite__head">
+      <template v-slot:before>
+        <router-link :to="{ name: 'home' }"><IconBack /></router-link>
+      </template>
+      Мои закладки
+    </Title>
+
+    <ProductCards :products="products" />
+  </div>
+</template>
+
+<style lang="scss">
+.favorite {
+  &__head {
+    padding-top: 40px;
+    padding-bottom: 40px;
+  }
+}
+</style>
