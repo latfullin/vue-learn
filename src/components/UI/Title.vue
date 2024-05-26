acc6499ab27ae4c9a5b28db85f9bc193a0090b2e
Тимур <kilkenny116@gmail.com>
2024-05-26 14:49:55 +0300

    component title

---
+++ src/components/UI/Title.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="title-component">
+    <slot name="before"></slot>
+    <h2 class="title">
+      <slot></slot>
+    </h2>
+    <slot name="after"></slot>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.title {
+  font-weight: 700;
+  font-size: 32px;
+  line-height: 38px;
+}
+
+.title-component {
+  display: flex;
+  align-items: center;
+  column-gap: 20px;
+}
+h2 {
+  margin: 0;
+}
+</style>
